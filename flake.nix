19a2c2335685e93c89796593c87061f382ed9e4c
Theofilos Augoustis <theofilos.augoustis@gmail.com>
2026-08-24 11:05:08 +0000

    Track peer endpoints on current Coyote develop

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
     coyote = {
-      url = "github:taugoust/Coyote/2b23475fac34d0e4650f52533d015377a9ae3217";
+      url = "github:taugoust/Coyote/5c31f6959aa9504dadab9688a31cf7819f145877";
       flake = false;
     };
   };
